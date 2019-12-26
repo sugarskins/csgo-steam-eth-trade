797b1107a0fcb77ffdbd21e78f3fd06c9988e5e4
Dan Octavian <danoctavian91@gmail.com>
2019-12-26 18:24:18 +0800

    remove ;

--- csgo-trade-frontend/src/utils.js
+++ csgo-trade-frontend/src/utils.js
@@ -29,12 +29,11 @@ function getTimeDifference(future, past) {
 
     // calculate (and subtract) whole days
     var days = Math.floor(delta / 86400)
-    delta -= days * 86400;
+    delta -= days * 86400
 
     // calculate (and subtract) whole hours
     var hours = Math.floor(delta / 3600) % 24
-    delta -= hours * 3600;
-
+    delta -= hours * 3600
     // calculate (and subtract) whole minutes
     var minutes = Math.floor(delta / 60) % 60
     delta -= minutes * 60
