7aa967157a2733d56f51c4470123e01fd040214e
Dan Octavian <danoctavian91@gmail.com>
2020-01-29 22:38:46 +0800

    try with functions as lib

--- csgo-trade-contract/contracts/CSGOSteamTrade.sol
+++ csgo-trade-contract/contracts/CSGOSteamTrade.sol
@@ -3,6 +3,7 @@ pragma experimental ABIEncoderV2;
 
 import "chainlink/contracts/ChainlinkClient.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./CSGOSteamTradeLib.sol";   
 
 
 contract CSGOSteamTrade is ChainlinkClient, Ownable {
@@ -170,7 +171,7 @@ contract CSGOSteamTrade is ChainlinkClient, Ownable {
         req.add("tradeURL", listing.purchaseOffer.buyerTradeURL);
         req.add("wear", listing.wear);
         req.add("skinName", listing.skinName);
-        req.add("paintSeed", uint2str(listing.paintSeed));
+        req.add("paintSeed", CSGOSteamTradeLib.uint2str(listing.paintSeed));
         
         string[] memory path = new string[](1);
         path[0] = "containsItem";
@@ -228,25 +229,4 @@ contract CSGOSteamTrade is ChainlinkClient, Ownable {
         cancelChainlinkRequest(_requestId, _payment, this.fulfillItemTransferConfirmation.selector, _expiration);
     }
 
-    /**
-     * @notice Converts a uint to a string. Extracted from https://github.com/provable-things/ethereum-api/blob/master/oraclizeAPI_0.5.sol
-     */
-    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
-        if (_i == 0) {
-            return "0";
-        }
-        uint j = _i;
-        uint len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint k = len - 1;
-        while (_i != 0) {
-            bstr[k--] = byte(uint8(48 + _i % 10));
-            _i /= 10;
-        }
-        return string(bstr);
-    }
 }
---
+++ csgo-trade-contract/contracts/CSGOSteamTradeLib.sol
@@ -0,0 +1,23 @@
+library CSGOSteamTradeLib {
+    /**
+     * @notice Converts a uint to a string. Extracted from https://github.com/provable-things/ethereum-api/blob/master/oraclizeAPI_0.5.sol
+     */
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = byte(uint8(48 + _i % 10));
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+}
